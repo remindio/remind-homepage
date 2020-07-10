731f9c694fff7fa27fa8f41ca54ec67cbb20df14
Daniel Leal <os.leal90@gmail.com>
2020-07-10 17:23:33 -0300

    Create homepage project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './assets/Remind.svg'
+import homeImage from './assets/home_image.svg'
+import environmentListImage from './assets/environment_list-image.svg'
+import environmentsImage from './assets/environments_image.svg'
 
-function App() {
+import './App.css'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="home-page">
+      <header>
+        <nav>
+          <img src={logo} style={{ width: 100, height: 64 }} alt="Remind"/>
+          <div>
+            <div className="about-container">
+              <a href="#">About</a>
+            </div>
+            <div>
+              <a href="http://localhost:3333/users/sign_in">Login</a>
+              <a href="http://localhost:3333/users/sign_up">Sign up</a>
+            </div>
+          </div>
+        </nav>
       </header>
+      <main>
+        <section className="main-section">
+          <div className="main-description">
+            <h1>The complete productivity environment</h1>
+            <div className="main-subdescription">
+              <p>Everything you need, in one place</p>
+              <button>Get Started</button>
+            </div>
+          </div>
+          <img src={homeImage} style={{ width: 500, height: 400 }} alt="Home image"/>
+        </section>
+        <section className="simple-section">
+          <div className="simple-section-first">
+            <div>
+              <h1>It's very simple</h1>
+            </div>
+            <div>
+              <h2>Create environments</h2>
+              <p>As many as you want, for everything that you need. A environment is where your information is organized.</p>
+              <img src={environmentListImage} alt=""/>
+            </div>
+            <div>
+              <img src={environmentsImage}></img>
+              <h2>Take notes and create tasks</h2>
+              <p>Organize your tasks in a list or create a custom annotation to remember a important thing.</p>
+            </div>
+          </div>
+          <div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+
+          </div>
+          <div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+
+          </div>
+        </section>
+        <section className="important-section">
+          <div className="section-title"></div>
+          <div>
+
+          </div>
+        </section>
+        <section className="last-section">
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </section>
+        
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
-
-export default App;
