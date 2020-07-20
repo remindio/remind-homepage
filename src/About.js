45c8f0d6353c62a6dd88e31dd6d825da9e8befe0
Daniel Leal <os.leal90@gmail.com>
2020-07-20 16:00:58 -0300

    Feat about page responsive

--- src/About.js
+++ src/About.js
@@ -3,6 +3,7 @@ import logo from './assets/Remind.svg'
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 import './About.css'
+import './About_media.css'
 
 export default function About() {
   return (
@@ -27,16 +28,18 @@ export default function About() {
         </nav>
       </header>
       <main>
-        <div className="main-section">
-          <div className="content">
-            <h1 className="mission">Our mission</h1>
-            <p>We're bulding Remindio to help individuals improve their daily productivity by giving them the tool to organize their life and responsibilities.</p>
+        <section className="main-section">
+          <div className="main-section-content">
+            <div className="content">
+              <h1 className="mission">Our mission</h1>
+              <p>We're bulding Remindio to help individuals improve their daily productivity by giving them the tool to organize their life and responsibilities.</p>
+            </div>
+            <div className="content">
+              <h1 className="about">About us</h1>
+              <p>We are two Computer Science undergraduates, Erick and Daniel, and we want Remindio to aid you in your mission to become better at what you do.</p>
+            </div>
           </div>
-          <div className="content">
-            <h1 className="about">About us</h1>
-            <p>We are two Computer Science undergraduates, Erick and Daniel, and we want Remindio to aid you in your mission to become better at what you do.</p>
-          </div>
-        </div>
+        </section>
       </main>
     </div>
   )
